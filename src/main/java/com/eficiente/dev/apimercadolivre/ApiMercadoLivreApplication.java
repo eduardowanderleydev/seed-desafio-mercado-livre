06991777b4882a0121989f87d491da16ca32347c
Eduardo Wanderley <eduardo.wanderley@alura.com.br>
2023-02-15 16:18:39 -0300

    initial commit

---
+++ src/main/java/com/eficiente/dev/apimercadolivre/ApiMercadoLivreApplication.java
@@ -0,0 +1,13 @@
+package com.eficiente.dev.apimercadolivre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMercadoLivreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiMercadoLivreApplication.class, args);
+    }
+
+}
